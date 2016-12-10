1eff5a365342202cfa85906e7641978255abf6fe
chrisbacon <bacon.chris@gmail.com>
2016-12-10 13:19:40 +0000

    removed an optimisation that probably didn't optimise anything, just made things confusing

--- cardGame/Hand.java
+++ cardGame/Hand.java
@@ -4,25 +4,18 @@ import java.util.*;
 
 public class Hand extends CardCollection {
 
-    Integer value;
-
     public Hand() {
         super();
-        this.value = null;
     }
 
     public void returnCardsToDealer(Dealer dealer){
         while (getSize() > 0) {
             Card cardRemoved = this.cards.remove(0);
             dealer.addCardToDeck(cardRemoved); 
-            value = null;
         }       
     }
 
     public int getValue() {
-        if (value == null) {
-            return Rules.getHandValue(this.cards);
-        }
-        return value;
+        return Rules.getHandValue(this.cards);
     }
 }
